ecd72a15f04397824f4b5c6232a790af05467300
sh940701 <visioner2168@gmail.com>
2023-12-21 15:16:38 +0900

    feat: add client server

---
+++ services/grpc-node-js/client.js
@@ -0,0 +1,18 @@
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+import * as process from 'node:process'
+
+// Protobuf 컴파일
+const packageDefinition = protoLoader.loadSync('../../packages/protos/user.proto')
+const packageObject = grpc.loadPackageDefinition(packageDefinition)
+
+// Client 생성
+const client = new packageObject.UserService('localhost:50051', grpc.credentials.createInsecure())
+client.getUserById({id: '1234'}, (error, response) => {
+    if (error) {
+        console.error(error)
+        process.exit(1)
+    }
+
+    console.log(response)
+})
