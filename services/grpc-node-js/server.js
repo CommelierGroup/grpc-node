87ff2d1205f80ce8bcec97e5d73ba37562f4cc8b
sh940701 <visioner2168@gmail.com>
2023-12-21 14:53:33 +0900

    feat: grpc  서버 구현

---
+++ services/grpc-node-js/server.js
@@ -0,0 +1,31 @@
+import grpc from '@grpc/grpc-js'
+import protoLoader from '@grpc/proto-loader'
+
+// 1. Protobuf 불러오기
+const packageDefinition = protoLoader.loadSync('../../packages/protos/user.proto')
+const packageObject = grpc.loadPackageDefinition(packageDefinition)
+
+// 2. grpc 서버 생성
+const server = new grpc.Server()
+
+// 3. 서비스 추가
+server.addService(packageObject.UserService.service, {
+    getUserById: (call, callback) => {
+        const userId = call.request.id
+        const user = getUserById(userId)
+
+        // 첫번째 인수: 에러
+        // 두번째 인수: 실제 데이터
+        callback(null, user)
+    }
+})
+
+function getUserById(id) {
+    return {id, name: 'John Doe', age: 20}
+}
+
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start()
+
+    console.log('Server started')
+})
